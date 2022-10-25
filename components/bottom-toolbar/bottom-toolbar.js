896e6578f74ff45c060b376f8fd792b0d58d8cd2
Coline Renuy <renuy.coline@gmail.com>
2022-10-25 12:02:08 +0200

    finished footbar. Waiting to add shadows

--- components/bottom-toolbar/bottom-toolbar.js
+++ components/bottom-toolbar/bottom-toolbar.js
@@ -2,13 +2,14 @@ import { TouchableOpacity, View, Text } from 'react-native';
 import styles from './styles.css';
 import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 
 export default function BottomToolbar({}) {
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
-        
+      <TouchableOpacity style = {styles.searchBtn}>
+        <AntDesign name='search1' size={35} color='black' />
       </TouchableOpacity>
       <View style={styles.footer}>
         <View style={styles.icons1}>

28cb21810e0f1ec66f3d0f3c3bc842f3a14394da
Manon <m.ventura@vatel.fr>
2022-10-27 17:07:28 +0200

    fin fiche produit en dur manque que css sur certains elements

--- screens/product/ProductScreen.js
+++ screens/product/ProductScreen.js
@@ -1,9 +1,9 @@
-import {ListHeaderComponent, ListFooterComponent, SafeAreaView, View, FlatList, Text, ImageBackground, TouchableOpacity, Modal} from 'react-native';
+import {Animated, createAnimatedComponent,SafeAreaView, View, FlatList, Text, ImageBackground, TouchableOpacity, Modal} from 'react-native';
 import styles from './style.css';
 import { useTheme } from '@react-navigation/native';
 import { useDispatch } from 'react-redux';
 import { loadFonts } from '../../assets/fonts/fonts';
-import { useState, useEffect, useCallback } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import BottomToolbar from '../../components/bottom-toolbar/bottom-toolbar';
 
 import { ScrollView } from 'react-native-gesture-handler';
@@ -14,12 +14,27 @@ import Cross from '../../components/icons/cross';
 import Scroll from '../../components/icons/scrollDown';
 
 export default function ProductScreen() {
+  //collapsible header 
+  // const scrollY = new Animated.Value(0);
+  // const diffClamp = Animated.diffClamp(scrollY, 0,405)
+  // const translateY = diffClamp.interpolate({
+  //   inputRange:[0,300],
+  //   outputRange:[0,-300]
+  // })
+
   const loadedFonts = loadFonts();
   //fait apparaître / disparaître la Modal
   const [modalVisible, setModalVisible] = useState(false);
+  const [detailedProgram, setDetailedProgram] = useState(3);
   if (!loadedFonts) return <></>;
-
+/* ---------------- A REMPLACER PAR UN FETCH  DEBUT ----------------  */
+  //image background url
   const image = { uri: 'https://res.cloudinary.com/dxq6tt9ur/image/upload/v1666688082/balibackground_zhvjoa.jpg' };
+  //images caroussel
+  const photos = ['https://res.cloudinary.com/dxq6tt9ur/image/upload/v1666688082/bali3_t80vuq.jpg', 
+  'https://res.cloudinary.com/dxq6tt9ur/image/upload/v1666688082/Bali1_bat19s.jpg', 
+  'https://res.cloudinary.com/dxq6tt9ur/image/upload/v1666688082/bali2_x92gur.jpg']
+
   const program = [
     {
       nbday: 3,
@@ -52,18 +67,59 @@ export default function ProductScreen() {
       ],
     },
   ];
+ const taags = ['Culture', 'Desert', 'Flight included', 'Trek', 'Hotel', 'Latin America']
 
-  const buttons = program.map((data, i) => {
+ /* ---------------- A REMPLACER PAR UN FETCH  FIN ----------------  */
+
+
+// to display buttons for programs
+const programSetter = (data) => {
+//selon le jour cliqué, afficher le detailed program correspondant
+  setDetailedProgram(data.nbday)
+}
+
+const goodProgram = program.find(program => program.nbday === detailedProgram)
+console.log(goodProgram)
+const programDisplay = goodProgram.detailedProgram.map((day, i) => {
+  return(
+    <View>
+      <Text>Day {day.day}</Text>
+      <Text>Activities: {day.activities}</Text>
+    </View>
+  )
+ })
+
+
+// DISPLAY NBDAYS PROGRAM FORMAT BOUTON 
+  const nbDaysButtons = program.map((data, i) => {
     return (
-      <TouchableOpacity style={styles.nbDaysModal} key={i}>
+      <TouchableOpacity 
+      onPress={() => programSetter(data)} 
+       style={styles.nbDaysModal} key={i}>
         <Text>{data.nbday} days</Text>
       </TouchableOpacity>
     );
   });
 
+// DISPLAY TAGS FORMAT BOUTON //? EST CE QU'ON FAIT QUE LES TAGS SOIENT CLIQUABLES ET RENVOIE A SEARCH PAR CE TAG LA?
+
+  const tags = taags.map((data, i) => {
+    return (
+      <TouchableOpacity
+      //  onPress={() => programDisplay(data)} 
+       style={styles.tagsModal} key={i}>
+        <Text>{data}</Text>
+      </TouchableOpacity>
+    );
+  });
+
+/* ---------------- CE QUI EST COMMENTE DANS LE RETURN CEST POUR LE COLLAPSIBLE HEADER ---------------- */
   return (
-    <View style={styles.scrollView}>
+    <View style={styles.scrollView}> 
+{/* ---------------- LANDING PAGE PHOTO BACKGROUND + INFOS PRINCIPALES ---------------- */}
       <ImageBackground style={styles.landing} source={image} resizeMode='cover'>
+
+{/* ---------------- HEADER BOUTONS LIKE ET RETOUR PAGE RECHERCHE ---------------- */}
         <View style={styles.header}>
           <TouchableOpacity style={styles.coeurBox}>
             <Coeur />
@@ -73,6 +129,7 @@ export default function ProductScreen() {
           </TouchableOpacity>
         </View>
 
+{/* ---------------- RECAP TRIP  ---------------- */}
         <View style={styles.recapTrip}>
           <Text style={styles.title}>Beaches and farniente</Text>
           <Text style={styles.text}>Bali </Text>
@@ -84,6 +141,7 @@ export default function ProductScreen() {
           <Scroll />
         </TouchableOpacity>
       </ImageBackground>
+{/* ---------------- MODAL = FICHE PRODUIT ---------------- */}
 
       <Modal
         animationType='slide'
@@ -93,8 +151,16 @@ export default function ProductScreen() {
         }}>
 
           <View  style={styles.modal}>
-              <ScrollView style={styles.scrollViewModal}>
-            <View style={styles.caroussel}>
+      <ScrollView
+      style={styles.scrollViewModal}>
+      {/* header modal */}
+        {/* <Animated.View style={{
+          transform:[{translateY:translateY}],
+          elevation: 4,
+          zIndex:-1,
+        }}> */}
+{/* ---------------- CAROUSSEL PHOTOS ---------------- */}
+          <View style={styles.caroussel}>
               <TouchableOpacity
                 name='close'
                 style={styles.crossBoxModal}
@@ -103,7 +169,16 @@ export default function ProductScreen() {
                 onPress={() => setModalVisible(!modalVisible)}>
                 <Cross />
               </TouchableOpacity>
-            </View>
+          </View>
+        {/* </Animated.View> */}
+        {/* body modal */}
+              {/* // scrollEventThrottle={16}
+              // onScroll={(e) =>{ */}
+              {/* //   console.log("e.nativeEvent.contentOffset.y",e.nativeEvent.contentOffset.y,"scrollY",scrollY,"diffClamp",diffClamp);
+              //     scrollY.setValue(e.nativeEvent.contentOffset.y)}} */}
+
+{/* ---------------- HEADER INFOS PRINCIPALES ---------------- */}
+
             <View style={styles.headerModal}>
               <Text style={styles.title}>Beaches and farniente</Text>
               <TouchableOpacity style={styles.heartBoxModal}>
@@ -111,7 +186,7 @@ export default function ProductScreen() {
               </TouchableOpacity>
             </View>
             <View style={styles.infoContainerModal}>
-              <FlatList
+              <Animated.FlatList
                 data={[
                   { key: 'Bali' },
                   { key: 'Séjour de 12 jours - Max 27 jours' },
@@ -122,9 +197,9 @@ export default function ProductScreen() {
               />
               <Text style={styles.offeredByModal}>Offered by EZTRIP</Text>
             </View>
-
+{/* ---------------- INCLUDED/NOT INCLUDED ---------------- */}
             <Text style={styles.smallTitle}>Included :</Text>
-            <FlatList
+            <Animated.FlatList
               columnWrapperStyle={{ flexWrap: 'wrap', flex: 1 }}
               numColumns={3}
               data={[
@@ -139,12 +214,14 @@ export default function ProductScreen() {
             />
 
             <Text style={styles.smallTitle}>Not included :</Text>
-            <FlatList
+            <Animated.FlatList
               columnWrapperStyle={{ flexWrap: 'wrap', flex: 1 }}
               numColumns={3}
               data={[{ key: 'Les pourboires' }, { key: 'Les boissons' }]}
               renderItem={({ item }) => <Text style={styles.inclusModal}>{item.key}</Text>}
             />
+{/* ---------------- MAP LOCALISATION ---------------- */}
+
             <Text style={styles.smallTitle}>Localisation :</Text>
             <MapView
               style={styles.map}
@@ -155,6 +232,10 @@ export default function ProductScreen() {
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
               }}></MapView>
+{/* ---------------- DESCRIPTION TRIP ---------------- */}
+{/* si on a le temps voir pour un "showmore"/"showless" pour pas avoir des descriptions a rallonge */}
+
+
             <Text style={styles.smallTitle}>Description :</Text>
             <Text numberOfLines={5} ellipsizeMode='tail' style={styles.inclusModal}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi
@@ -163,11 +244,34 @@ export default function ProductScreen() {
               tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia
               aut!{' '}
             </Text>
-            <View style={styles.nbDaysContainer}>{buttons}</View>
 
-            {/* ALL THE CODE HAS TO GO OVER THIS LINE */}
+{/* ---------------- PROGRAMME ---------------- */}
+            <Text style={styles.smallTitle}>Program :</Text>
+            <View style={styles.nbDaysContainer}>{nbDaysButtons}</View>
+            {programDisplay}
+
+{/* ---------------- TAGS ---------------- */}
+
+            <Text style={styles.smallTitle}>Tags :</Text>
+            <View style={styles.tagsContainer} >{tags}</View>
+            <View style={styles.buttonsContainer}>
+
+{/* ---------------- BOUTONS QUOTATION ET DOWNLOAD ---------------- */}
+
+              <TouchableOpacity style={styles.quotationButton}>
+                <Text style={styles.buttonText}>Quotation request</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.programButton}>
+                <Text style={styles.buttonText}>Download program (PDF)</Text>
+              </TouchableOpacity>
+            </View>
+
+{/* ---------------- ALL THE CODE HAS TO GO OVER THIS LINE ---------------- */}
+
+{/* ---------------- FOOTER BOTTOM BAR ---------------- */}
             <View style={{ height: 90 }}></View>
           </ScrollView>
+        {/* </ScrollView> */}
         </View>
         <BottomToolbar></BottomToolbar>
       </Modal>

0ea6169b3388eb42fcaae3488d28e2fc1f21c6f1
Craig Chick <craigchick@outlook.com>
2022-11-01 17:14:31 +0100

    Animated progress route

--- screens/next_step/NextStep.js
+++ screens/next_step/NextStep.js
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
+import { useRef, useEffect } from 'react';
 import styles from './style.css';
+import BackgroundImageLayer from '../../components/background_image_layer/BackgroundImageLayer';
+import mountImg from '../../assets/images/Mountain.png';
+import BottomToolbar from '../../components/bottom-toolbar/bottom-toolbar';
+import RouteSvg from './RouteSvg/RouteSvg';
+import AnimatedProgressRoute from './RouteSvg/Trip_Progress_Route';
 
 export default function NexStep() {
+  const animatedSvgRef = useRef();
+
   return (
-    <View style={styles.mainContainer}>
-      <Text>Hello</Text>
-    </View>
+    <>
+      <BackgroundImageLayer source={mountImg} layerOpacity={0.1} style={styles.imageBackground} />
+      <View
+        style={{
+          ...styles.mainContainer,
+        }}>
+        <AnimatedProgressRoute ref={(ref) => (animatedSvgRef.current = ref)} />
+      </View>
+      <BottomToolbar />
+    </>
   );
 }
---
+++ screens/next_step/RouteSvg/AnimatedProgressRoute.js
@@ -0,0 +1,6 @@
+import SVGatorComponent from './Trip_Progress_Route';
+import { useRef, useEffect } from 'react';
+
+export default function AnimatedProgressRoute() {
+  return <SVGatorComponent />;
+}

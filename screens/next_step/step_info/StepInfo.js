4b4058f78b69a7aa13754b170d8d4ad62f2ab835
Craig Chick <craigchick@outlook.com>
2022-11-02 11:35:26 +0100

    Adjusting progress path

--- screens/next_step/step_info/StepInfo.js
+++ screens/next_step/step_info/StepInfo.js
@@ -1,27 +1,50 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, Modal } from 'react-native';
+import { useState, useEffect } from 'react';
 import styles from './style.css';
 import { useTheme } from '@react-navigation/native';
-import OpenCrossBtn from '../../../components/close_button/CloseButton';
+import CrossBtn from '../../../components/close_button/CloseButton';
 import FadeContainer from '../../../components/fade_container/FadeContainer';
 
-export default function StepInfo({ title, step, currentStep, containerStyle, onIconPress }) {
+export default function StepInfo({ title, step, currentStep, containerStyle }) {
   const { nextStep } = useTheme();
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
-    <FadeContainer
-      style={{ ...styles.infoContainer, ...containerStyle }}
-      isVisible={step === currentStep ? true : false}>
-      <View style={styles.textContainer}>
-        <Text style={{ ...styles.infoTitle, color: nextStep.stepInfoText }}>{title}</Text>
-      </View>
-      <View style={styles.btnContainer}>
-        <OpenCrossBtn
-          iconColor={nextStep.stepInfoText}
-          iconScale={0.3}
-          iconStyle={{ padding: 6, transform: [{ rotate: '45deg' }] }}
-          onPress={onIconPress}
-        />
-      </View>
-    </FadeContainer>
+    <>
+      <FadeContainer
+        style={{ ...styles.infoContainer, ...containerStyle }}
+        isVisible={step === currentStep ? true : false}>
+        <View style={styles.textContainer}>
+          <Text style={{ ...styles.infoTitle, color: nextStep.stepInfoText }}>{title}</Text>
+        </View>
+        <View style={styles.btnContainer}>
+          <CrossBtn
+            iconColor={nextStep.stepInfoText}
+            iconScale={0.3}
+            iconStyle={{ margin: 10, transform: [{ rotate: '45deg' }] }}
+            onPress={() => setModalVisible(true)}
+          />
+        </View>
+      </FadeContainer>
+      <Modal
+        transparent={true}
+        presentationStyle={'overFullScreen'}
+        visible={modalVisible}
+        animated={true}
+        animationType={'fade'}
+        statusBarTranslucent={true}>
+        <View style={{ ...styles.contentContainer }}>
+          <View style={{ ...styles.contentWrapper, backgroundColor: nextStep.modalBg }}>
+            <CrossBtn
+              style={styles.modalCloseBtn}
+              iconColor={nextStep.modalCloseBtn}
+              iconScale={0.4}
+              onPress={() => setModalVisible(false)}
+            />
+            <Text>Info Modal</Text>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
